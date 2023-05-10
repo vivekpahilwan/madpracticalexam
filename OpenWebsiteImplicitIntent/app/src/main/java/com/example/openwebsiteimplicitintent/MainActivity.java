3332ede30ad106824dc172459cd243b29660833d
vivekpahilwan <vivekpahilwan@gmail.com>
2023-05-10 19:29:43 +0530

    website

--- OpenWebsiteImplicitIntent/app/src/main/java/com/example/openwebsiteimplicitintent/MainActivity.java
+++ OpenWebsiteImplicitIntent/app/src/main/java/com/example/openwebsiteimplicitintent/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.openwebsiteimplicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     EditText search_bar;
     Button btn;
+    Intent obj = new Intent(Intent.ACTION_VIEW);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
 
         search_bar = findViewById(R.id.search_bar);
         btn = findViewById(R.id.btn);
+
+
+        obj.setData(Uri.parse("www.google.com"));
     }
 
+    public void openweb(View v){
+        startActivity(obj);
+    }
 }
