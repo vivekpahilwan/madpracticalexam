eadbaa414f979b883c5a231191887e03ade27edd
vivekpahilwan <vivekpahilwan@gmail.com>
2023-05-10 17:19:43 +0530

    calculator and website

---
+++ OpenWebsiteImplicitIntent/app/src/main/java/com/example/openwebsiteimplicitintent/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.openwebsiteimplicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText search_bar;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        search_bar = findViewById(R.id.search_bar);
+        btn = findViewById(R.id.btn);
+    }
+
+}
